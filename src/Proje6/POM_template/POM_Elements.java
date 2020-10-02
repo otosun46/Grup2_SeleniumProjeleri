3da1016a434a0390548f9bf5f7f89e4a73e61c24
Otosun <64793578+otosun46@users.noreply.github.com>
2020-10-02 19:22:17 +0200

    TestNG tanimlandi

---
+++ src/Proje6/POM_template/POM_Elements.java
@@ -0,0 +1,20 @@
+/**
+ * @Author:Otosun Tarih :29/09/2020
+ */
+package Proje6.POM_template;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class POM_Elements {
+    public POM_Elements(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(css=".abc")
+        public WebElement abc;
+
+    @FindBy(xpath="//a[@clas='def']")
+    public WebElement def;
+}
